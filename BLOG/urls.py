6587d3306a7725c4a18b4ab22bdc24c1ca6f9e4b
Gaenox <blue3eagle3naro@gmail.com>
2021-10-30 22:25:16 +0200

    done password reset

--- BLOG/urls.py
+++ BLOG/urls.py
@@ -1,7 +1,6 @@
-from django.contrib.auth import login
 from django.urls import path
-from .views import Userlogout, home, Userregister, Userlogin
-
+from .views import Userlogout, about, home, Userregister, Userlogin, profile, reset_password
+from django.contrib.auth import views
 #paths
 
 
@@ -10,4 +9,11 @@ urlpatterns = [
   path("register/", Userregister, name="register"),
   path("login/", Userlogin, name="login"),
   path("logout/", Userlogout, name="logout"),
+  path("profile/", profile, name="profile"),
+  path("about/", about, name="about"),
+    # password reset
+  path('password_reset/done/', views.PasswordResetDoneView.as_view(template_name='BLOG/password_reset_templates/password_reset_done.html'), name='password_reset_done'),
+  path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(template_name="BLOG/password_reset_templates/password_reset_confirm.html"), name='password_reset_confirm'),
+  path('reset/done/', views.PasswordResetCompleteView.as_view(template_name='BLOG/password_reset_templates/password_reset_complete.html'), name='password_reset_complete'),
+  path('password_reset/', reset_password, name="password_reset"),
 ]
--- BLOG/views.py
+++ BLOG/views.py
@@ -1,7 +1,16 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
+from django.http.response import BadHeaderError, HttpResponse
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 from .forms import UserCreationForm
+from django.db.models.query_utils import Q
+from .models import User
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.template.loader import render_to_string
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib import messages
 
 
 # Create your views here.
@@ -25,7 +34,7 @@ def Userregister(request):
   }
   return render(request, 'BLOG/register.html', context)
 
-def Userlogin(request): # not done
+def Userlogin(request):
   form =AuthenticationForm()
   if request.method == "POST":
     form=AuthenticationForm(request, data=request.POST)
@@ -39,3 +48,45 @@ def Userlogin(request): # not done
 def Userlogout(request):
   logout(request)
   return redirect('home')
+
+def profile(request):
+  data={
+    "data": request.user
+  }
+  return render(request, 'BLOG/profile.html', context=data)
+
+def about(request):
+  return render(request, 'BLOG/about.html',)
+
+def reset_password(request):
+  form=PasswordResetForm(request.POST)
+  if request.method == "POST":
+    form=PasswordResetForm(request.POST)
+    if form.is_valid():
+      data = form.cleaned_data['email']
+      users = User.objects.filter((Q(email=data)))
+      if users.exists():
+        for user in users:
+          subject = "Password Reset Requested"
+          email_template = "BLOG/password_reset_templates/password_reset_email.txt"
+          c={
+            "email":user.email,
+					  'domain':'127.0.0.1:8000',
+					  'site_name': 'Djangalactic Blog',
+					  "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					  "user": user,
+					  'token': default_token_generator.make_token(user),
+					  'protocol': 'http',
+          }
+          email=render_to_string(email_template, c)
+          try:
+            send_mail(subject, email, "DjangalacticTeam@gmail.com", [user.email],fail_silently=False)
+          except BadHeaderError:
+            return HttpResponse('Invalid header found')
+          messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
+          return redirect("done/")
+      messages.error(request, 'An invalid email has been entered.')
+  context ={
+    "form":form
+  }
+  return render(request, 'BLOG/password_reset_templates/password_reset.html',context)
